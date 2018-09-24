3d619bb3d07504f761eaadaf5f688b9b92abffb6
Raghul R Nair <raghulunni@gmail.com>
2018-09-24 11:11:07 +0530

    no message

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,8 +1,5 @@
 import {combineReducers} from 'redux';
-// import {routerReducer} from 'react-router-redux';
-// import {reducer as formReducer} from 'redux-form';
 // import DemoReducer from './modules/demo/reducer';
-// import SuperStaffReducer from './modules/Superstaff/reducer';
 
 export default combineReducers({
  
